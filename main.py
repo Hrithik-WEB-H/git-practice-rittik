444192c5f4ef518498397e39c63c77268d68fe07
Hrithik-Web-H <shhalder213@gmail.com>
2026-05-01 17:02:12 +0600

    Add Multiplication function

--- main.py
+++
@@ -1,4 +0,0 @@
-from datetime import date
-
-print("Name: Rittik Halder")
-print("Today's Date: ", date.today())
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiplication
 
 print("Name: Rittik Halder")
 print("Today's Date: ", date.today())
 
 print("Addition: ", add(5,3))
 print("Subtraction: ", subtract(5, 3))
+print("Multiplication: ", multiplication(5, 3))
